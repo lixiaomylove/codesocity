e408436da44fb6bf15bdca21fbc9ed022cdde297
lixiaomylove <75514057+lixiaomylove@users.noreply.github.com>
2020-12-18 07:21:38 +0800

    Add files via upload

---
+++ C语言100道——i+100&&i+268开平方.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    long int i,x,y,z;
+    for(i=1;i<100000;i++)
+    {
+        x=sqrt(i+100);
+        y=sqrt(i+268);
+        if(x*x==i+100 && y*y==i+268)
+            printf("\n%ld\n",i);
+
+    }
+    return 0;
+}
+
